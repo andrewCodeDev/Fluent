f4d3728fd420fef7104305ce78ee28afad1c5892
PierreLgol <plgol.perso@gmail.com>
2024-04-13 13:06:24 +0000

    added SIMD implementation of scalar spn

--- fluent.zig
+++ fluent.zig
@@ -505,6 +505,27 @@ fn simdCspan(comptime T: type, v: T, items: []T) usize {
     return (i);
 }
 
+fn simdSpan(comptime T: type, v: T, items: []T) usize {
+    var i: usize = 0;
+
+    if (comptime std.simd.suggestVectorLength(T)) |N| {
+        const VEC = @Vector(N, T);
+        const mask: VEC = @splat(v);
+
+        while ((i + N) <= items.len) : (i += N) {
+            const block: *const VEC = @ptrCast(@alignCast(items[i..][0..N]));
+            const result = block.* == mask;
+            if (@reduce(.And, result)) {
+                return (i + @as(usize, (@intCast(std.simd.firstTrue(result) orelse N))));
+            }
+        }
+    }
+    while (i < items.len) : (i += 1) {
+        if (items[i] != v) return (i);
+    }
+    return (i);
+}
+
 // these work for @Vector as well as scalar types
 inline fn maxGeneric(x: anytype, y: anytype) @TypeOf(x) {
     return @max(x, y);
