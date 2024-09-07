23d1998f2df21a9d498e6c8028b719d7cf5dbed0
PierreLgol <plgol.perso@gmail.com>
2024-09-07 10:35:11 +0200

    added a check step to improve lsp performance

--- build.zig
+++ build.zig
@@ -13,6 +13,17 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
+    const check = b.addStaticLibrary(.{
+        .name = "Fluent",
+        .root_source_file = b.path("fluent.zig"),
+        .target = target,
+        .optimize = optimize,
+        .use_llvm = false,
+        .use_lld = false,
+    });
+    const check_step = b.step("check", "check if the code compiles");
+    check_step.dependOn(&check.step);
+
     _ = b.addModule("Fluent", .{
         .root_source_file = b.path("fluent.zig"),
         .target = target,
